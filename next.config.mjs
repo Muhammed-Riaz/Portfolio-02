d6cdeb117284fe0230e1bdd5308869bccc44c5e7
Riaz zaur <riazahmedzaur110@gmail.com>
2025-02-02 23:18:52 +0500

    added Email

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,13 @@
-
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images:{
-//     domains:["cdn0.iconfinder.com","www.shareicon.net", "en.vetores.org","www.pngitem.com","cdn.freebiesupply.com","images.ctfassets.net","logodownload.org","upload.wikimedia.org"]
-//   }
-// };
-
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https", // The protocol used by your image URLs
-        hostname: "cdn.sanity.io", // Sanity's image CDN hostname
-        pathname: "/images/**", // Match all images under the `/images` path
+        protocol: 'https',
+        hostname: '**', // Allows all remote images (or use specific hostnames)
       },
-      
     ],
   },
-}
+};
 
 export default nextConfig;
